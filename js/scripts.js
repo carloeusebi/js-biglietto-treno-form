8914b8fab7fe958918b3665f8d24a8fbda2e6cf0
carloeusebi <carloeusebi@gmail.com>
2023-05-29 13:55:39 +0200

    Calculated base ticket price

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,18 @@ const tempAnswer = document.querySelector('.temporary-answer');
 const pricePerKm = 0.21;
 const discountJunior = 0.8;
 const discountSenior = 0.6;
+
+button.addEventListener('click', function(){
+    const passengerName = elementName.value.trim();
+    const passengerAge = elementAge.value;
+    const kilometers = parseInt(elementKm.value);
+    
+    console.log(passengerName, passengerAge, kilometers);
+
+    //TODO add validation
+
+    let basePrice = kilometers * pricePerKm;
+    
+    console.log(basePrice);
+
+})
