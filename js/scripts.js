bb43140814dd4c20929efe7eb7e16dea44a47cff
carloeusebi <carloeusebi@gmail.com>
2023-05-29 16:51:01 +0200

    Refactored scripts, added functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,27 @@
 // Targeting dom elements
-
 const elementName = document.getElementById('name');
 const elementKm = document.getElementById('distance');
 const elementAge = document.getElementById('age');
+
 const submitButton = document.getElementById('submit');
 const resetButton = document.getElementById('reset');
-const tempAnswer = document.querySelector('.temporary-answer');
 
 const ticket = document.querySelector('.ticket');
 
 // declaring const variables
-
 const pricePerKm = 0.21;
 const discountJunior = 0.8;
 const discountSenior = 0.6;
 
 resetButton.addEventListener('click', function () {
+
+    //reset errors
+    resetErrors();
+
     //clear fields
     elementName.value = '';
     elementKm.value = '';
     elementAge.value = 'adulto';
-
-    //resets errors (should use function :D)
-    elementName.classList.remove('is-invalid');
-    elementKm.classList.remove('is-invalid');
-    elementAge.classList.remove('is-invalid');
-    document.getElementById('validate-name').style.display = 'none';
-    document.getElementById('validate-km').style.display = 'none';
 
     //hides ticket
     ticket.classList.add('hidden');
@@ -36,19 +31,56 @@ resetButton.addEventListener('click', function () {
 })
 
 submitButton.addEventListener('click', function () {
+
     // clean old errors
-    elementName.classList.remove('is-invalid');
-    elementKm.classList.remove('is-invalid');
-    elementAge.classList.remove('is-invalid');
-    document.getElementById('validate-name').style.display = 'none';
-    document.getElementById('validate-km').style.display = 'none';
+    resetErrors();
 
     // collect user inputs
     const passengerName = elementName.value.trim();
-    const passengerAge = elementAge.value;
     const kilometers = parseInt(elementKm.value);
+    const passengerAge = elementAge.value;
 
-    //# Validation
+    // Validation
+    let isValid = validateInputs(passengerName, passengerAge, kilometers);
+
+    if (!isValid) {
+
+        //if not valid hides old ticket
+        ticket.classList.add('hidden');
+    } else {
+
+        // MAIN LOGIC
+
+        let price = kilometers * pricePerKm;
+
+        if (passengerAge === 'junior') price *= discountJunior;
+        else if (passengerAge === 'senior') price *= discountSenior;
+
+        const ticketNumber = generateRndNumber(100000, 999999);
+        const cab = generateRndNumber(2, 9);
+        const seat = generateRndNumber(1, 99);
+
+        ticket.classList.remove('hidden'); //show ticket
+
+        // collect dom elements and print outputs
+        const ticketName = document.getElementById('ticket-name');
+        const ticketType = document.getElementById('ticket-type');
+        const ticketPrice = document.getElementById('ticket-price');
+        const ticketCab = document.getElementById('ticket-cab');
+        const ticketSeat = document.getElementById('ticket-seat');
+        const ticketCode = document.getElementById('ticket-code');
+
+        ticketName.innerText = passengerName;
+        ticketType.innerText = passengerAge;
+        ticketPrice.innerText = price.toFixed(2) + '€';
+        ticketCode.innerText = ticketNumber;
+        ticketCab.innerText = cab;
+        ticketSeat.innerText = seat;
+    }
+})
+
+function validateInputs(passengerName, passengerAge, kilometers) {
+
     let isValid = true;
 
     if (!passengerName) {
@@ -69,43 +101,17 @@ submitButton.addEventListener('click', function () {
         elementAge.classList.add('is-invalid');
     }
 
-    // ! AFTER validation 'grab' all dom element
+    return isValid;
+}
 
-    if (!isValid) {
-        //if not valid hides old ticket
-        ticket.classList.add('hidden');
-    } else {
+function resetErrors() {
+    elementName.classList.remove('is-invalid');
+    elementKm.classList.remove('is-invalid');
+    elementAge.classList.remove('is-invalid');
+    document.getElementById('validate-name').style.display = 'none';
+    document.getElementById('validate-km').style.display = 'none';
+}
 
-        const ticketName = document.getElementById('ticket-name');
-        const ticketType = document.getElementById('ticket-type');
-        const ticketPrice = document.getElementById('ticket-price');
-        const ticketCab = document.getElementById('ticket-cab');
-        const ticketSeat = document.getElementById('ticket-seat');
-        const ticketCode = document.getElementById('ticket-code');
-
-        let price = kilometers * pricePerKm;
-
-        //apply discount
-        if (passengerAge === 'junior') price *= discountJunior;
-        else if (passengerAge === 'senior') price *= discountSenior;
-
-        //# generate random ticket code, cab and seat;
-        // ticket number is between 100000 and 999999
-        const ticketNumber = Math.floor(Math.random() * 899999 + 100000);
-        // cab is a number between 2 and 9
-        const cab = Math.floor(Math.random() * 8 + 2);
-        // seat is a number between 1 and 99
-        const seat = Math.floor(Math.random() * 99 + 1);
-
-        // # print on screen
-        // show ticket
-        ticket.classList.remove('hidden');
-
-        ticketName.innerText = passengerName;
-        ticketType.innerText = passengerAge;
-        ticketPrice.innerText = price.toFixed(2) + '€';
-        ticketCode.innerText = ticketNumber;
-        ticketCab.innerText = cab;
-        ticketSeat.innerText = seat;
-    }
-})
+function generateRndNumber(min, max) {
+    return Math.floor(Math.random() * (max + 1 - min) + min);
+}
