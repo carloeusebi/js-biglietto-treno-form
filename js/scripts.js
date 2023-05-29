d2fe41505424357e1e2913fcb69101eeeb941f64
carloeusebi <carloeusebi@gmail.com>
2023-05-29 13:47:30 +0200

    Declared js dom elements and constants

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
-console.log('JSOK');
+// Targeting dom elements
+
+const passengerName = document.getElementById('name');
+const kilometers = document.getElementById('distance');
+const passengerAge = document.getElementById('age');
+const tempAnswer = document.querySelector('.temporary-answer');
+
+console.log(passengerAge, passengerName, kilometers, tempAnswer);
+
+// declaring const variables
+
+const pricePerKm = 0.21;
+const discountJunior = 0.8;
+const discountSenior = 0.6;
